abd5eefbe52c67755cd6a2befd501eabf72b92d0
ikaromm <ikaromm@hotmail.com>
2025-01-14 07:17:09 -0300

    Add Feature TimeSeries

--- src/main/__main__.py
+++ src/main/__main__.py
@@ -1,11 +1,16 @@
 from data_pipeline.data_pipeline import DataPipeline
 from machine_learn.machine_learn import Machine_Learn
+from time_series.time_series import TimeSeriesForecast
 def main():
   
     pipe_line = DataPipeline('teste')
     df = pipe_line.pipe_line()
+    
     ml = Machine_Learn(df)
     ml.machine_learn()
+
+    tm_series = TimeSeriesForecast(df, target_col="price_diff")
+    tm_series.run_pipeline(order=(1,1,1))
 
 
 
